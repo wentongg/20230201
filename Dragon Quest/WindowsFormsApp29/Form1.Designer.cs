6da11471e1e8da9cc71cd498133f7436fc6a26c1
wentongg <124119550+wentongg@users.noreply.github.com>
2023-02-01 15:46:41 +0800

    Add files via upload

---
+++ Dragon Quest/WindowsFormsApp29/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace WindowsFormsApp29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(392, 367);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(423, 84);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(389, 367);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "0.png");
+            this.imageList1.Images.SetKeyName(1, "1.png");
+            this.imageList1.Images.SetKeyName(2, "2.png");
+            this.imageList1.Images.SetKeyName(3, "d0.png");
+            this.imageList1.Images.SetKeyName(4, "d1.png");
+            this.imageList1.Images.SetKeyName(5, "d2.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(755, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(755, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(755, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(414, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(364, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 200;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(290, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 61);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "100";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(412, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 61);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(12, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 123);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "開始遊戲";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 99.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(42, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(727, 133);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "勇者鬥惡龍";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 146);
+            this.textBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Dragon Quest/WindowsFormsApp29/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        Random rd = new Random();
+       
+        int herostate = 0;
+        int dinosuarlife = 100;
+        int herolife = 100;
+        bool keyup = true;
+        bool start = false; //開始遊戲
+
+        public Form1()
+        {
+            InitializeComponent();
+            int[] dinosuar = new int[3];
+            int kk;
+            int dinosuarstate = 0;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            textBox1.ReadOnly = true; //不讓使用者輸入文字           
+            Random rd = new Random();           
+            Application.DoEvents();            
+            Application.DoEvents();
+            pictureBox2.Image = imageList1.Images[0];
+            label4.Text = "0";
+            label3.Text = dinosuarstate.ToString();               
+            System.Threading.Thread.Sleep(500);
+            Application.DoEvents();
+            textBox1.Visible = false; //先把訊息框隱藏
+            
+            if (pictureBox1.Image == imageList1.Images[3])
+            {
+                dinosuarstate = 2020220;
+                label3.Text = dinosuarstate.ToString();
+            }
+            
+
+        }
+
+        
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(start == true) //開始遊戲才能移動
+            {
+                int herostate = 0;
+                label1.Text = e.KeyCode.ToString();
+                label2.Text = e.KeyValue.ToString();
+                if (label2.Text == "65")
+                {
+                    pictureBox2.Image = imageList1.Images[2];
+                    herostate = 1;
+                    label4.Text = herostate.ToString();
+                    keyup = false;
+                }
+                else
+                {
+                    if (label2.Text == "83")
+                    {
+                        pictureBox2.Image = imageList1.Images[1];
+                        herostate = 2;
+                        label4.Text = herostate.ToString();
+                        keyup = false;
+                    }
+
+                }
+            }
+            
+           
+
+          
+
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e) //恐龍自動攻擊
+        {
+            int kk;
+            int dinosuarstate;
+            kk = rd.Next(3, 6);
+            pictureBox1.Image = imageList1.Images[kk];
+
+            if (kk == 3)
+            {
+                dinosuarstate = 0;
+                label5.Text = dinosuarstate.ToString();
+            }
+
+            if (kk == 4)
+            {
+                dinosuarstate = 1;
+                label5.Text = dinosuarstate.ToString();
+            }
+
+            if (kk == 5)
+            {
+                dinosuarstate = 2;
+                label5.Text = dinosuarstate.ToString();
+            }
+        }
+
+        
+
+        
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (dinosuarlife < 60)
+                label6.ForeColor = Color.Red;
+            if (herolife < 60)
+                label7.ForeColor = Color.Red;
+
+            if (label4.Text == "1"&&label5.Text == "0")
+            {
+                dinosuarlife -= 7;
+                label6.Text = dinosuarlife.ToString();
+                textBox1.AppendText("勇者對惡龍造成7點傷害" + "\r\n");
+            }
+            else
+            {
+                if(label4.Text == "1" && label5.Text == "1") //恐龍的防禦是1
+                {
+                    dinosuarlife -= 3;
+                    label6.Text = dinosuarlife.ToString();
+                    textBox1.AppendText("勇者對惡龍造成3點傷害" + "\r\n");
+                }
+                else
+                {
+                    if(label4.Text == "1" && label5.Text == "2")
+                    {
+                        dinosuarlife -= 0;
+                        label6.Text = dinosuarlife.ToString();
+                        textBox1.AppendText("惡龍抵擋下了攻擊" + "\r\n");
+                    }
+                }
+            }
+
+
+            if (label4.Text == "0" && label5.Text == "2")//恐龍2是攻擊
+            {
+                herolife -= 7;
+                label7.Text = herolife.ToString();
+                textBox1.AppendText("惡龍對勇者造成7點傷害" + "\r\n");
+            }
+            else
+            {
+                if (label4.Text == "1" && label5.Text == "2") //恐龍的防禦是1
+                {
+                    herolife -= 3;
+                    label7.Text = herolife.ToString();
+                    textBox1.AppendText("惡龍對勇者造成3點傷害" + "\r\n");
+                }
+                else
+                {
+                    if (label4.Text == "2" && label5.Text == "2")
+                    {
+                        herolife -= 0;
+                        label7.Text = herolife.ToString();
+                        textBox1.AppendText("勇者抵擋下了攻擊!" + "\r\n");
+                    }
+                }
+            }
+
+            DialogResult result;
+
+            if(Convert.ToInt32( label6.Text) <= 0)
+            {
+                textBox1.Visible = false;
+                textBox1.Clear();
+                label6.Text = 0.ToString();                                                         //讓血量=0
+                start = false;                                                                      //停止動作
+                timer5.Enabled = false;
+                timer1.Enabled = false;
+                MessageBox.Show( "勇者!", "The winner is");
+                timer5.Enabled = false;
+               
+                
+
+                result = MessageBox.Show("挑戰超究極?", "贏了!!!",MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    dinosuarlife = 100;
+                    herolife = 100;
+                    label6.Text = "100";
+                    label7.Text = "100";
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    timer1.Interval = 150;
+                    timer1.Enabled = true;
+                    timer5.Enabled = true;
+                    start = true;
+                    textBox1.Visible = true;
+                }
+                
+            }
+            else
+            {
+                if(Convert.ToInt32( label7.Text) <= 0)
+                {
+                    textBox1.Visible = false;
+                    textBox1.Clear();
+                    label7.Text = 0.ToString();
+                    start = false;
+                    timer5.Enabled = false;
+                    timer1.Enabled = false;
+                    MessageBox.Show( "惡龍!", "The winner is");
+                    timer5.Enabled = false;
+                    
+                    
+
+                    result = MessageBox.Show("再玩一場?", "輸了QQ", MessageBoxButtons.YesNo);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        dinosuarlife = 100;
+                        herolife = 100;
+                        label6.Text = "100";
+                        label7.Text = "100";
+                        label6.ForeColor = Color.Black;
+                        label7.ForeColor = Color.Black;
+                        timer1.Enabled = true;
+                        timer5.Enabled = true;
+                        start = true;
+                        textBox1.Visible = true;
+                    }
+                    
+                }
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            keyup = true;
+            pictureBox2.Image = imageList1.Images[0];
+            herostate = 0;
+            label4.Text = herostate.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = false;
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            this.Controls.Remove(button1);//按鈕會導致無法偵測鍵盤事件
+            timer1.Enabled = true;
+            timer5.Enabled = true;
+            start = true;
+            textBox1.Visible = true; //訊息框打開
+        }
+
+        
+    }
+}
